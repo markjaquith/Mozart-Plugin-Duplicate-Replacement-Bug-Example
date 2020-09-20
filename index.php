d2890fbcf93e1fabff128400c6860b4967354dd7
Mark Jaquith <mark@coveredwebservices.com>
2020-09-19 20:38:23 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Mozart Plugin Autoloader Bug Example
+Author: Mark Jaquith
+*/
+
+namespace MarkJaquith\MozartDuplicateReplacementBug;
+
+use MarkJaquith\MozartDuplicateReplacementBug\Mozart\DI\Container;
+
+require __DIR__ . '/vendor/autoload.php';
+
+add_action('init', function() {
+	// This call fails, because in DI/Container.php, a use statement that started like this:
+	//
+	// use DI\Invoker\DefinitionParameterResolver;
+	//
+	// and should be transformed to this:
+	//
+	// MarkJaquith\MozartDuplicateReplacementBug\Mozart\DI\Invoker\DefinitionParameterResolver
+	//
+	// is instead transformed to this:
+	//
+	// MarkJaquith\DuplicateReplacementBug\Mozart\DI\MarkJaquith\DuplicateReplacementBug\Mozart\Invoker\DefinitionParameterResolver
+	//
+	new Container();
+});
